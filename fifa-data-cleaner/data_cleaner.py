86387f3b4f176eb95fc1f63b8bf6b67afbb4945a
Tanay <tanayj2107@gmail.com>
2026-01-06 23:27:41 -0500

    concise code (for loops) and added a feature where if it ends with K it gets three 0's

--- fifa-data-cleaner/data_cleaner.py
+++ fifa-data-cleaner/data_cleaner.py
@@ -3,10 +3,21 @@ import pandas as pd
 class FIFADataCleaner:
 
     def __init__(self):
-        self.df = pd.read_csv('fifa_raw_data.csv', low_memory = False)
+        self.df = pd.read_csv('fifa-data-cleaner/fifa_raw_data.csv', low_memory = False)
 
         # Row Count
         self.count = len(self.df)
 
         #Empty dict.
         self.report = {}
+
+        print(self.df.head())
+
+    def clean_value_wage_clause(self):
+        #Removing Pound Symbol
+        for i in ['Value', 'Wage', 'Release Clause']:
+            self.df[i] = self.df[i].str.replace('€', '')
+        #Multiplying by ($)1000 if ends with K
+        for i in ['Value', 'Wage', 'Release Clause']:
+            self.df[i] = self.df[i].str.replace('K', '000')
+
